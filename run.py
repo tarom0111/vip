ee05e992bbc5effe1723b1956aef9bc9ae7e945e
tarom0111 <77558188+tarom0111@users.noreply.github.com>
2023-02-28 11:34:11 +0700

    Add files via upload

---
+++ run.py
@@ -0,0 +1,11 @@
+import sys, os
+
+if __name__ == "__main__":
+  try:os.system("git pull")
+  except:pass
+  try:os.mkdir("OK")
+  except:pass
+  try:os.mkdir("CP")
+  except:pass
+  __import__("rembo").menu()
+
